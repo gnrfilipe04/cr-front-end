2c40d069bddcfbdf4dcb53a0e6e4a997ca964fc1
gnrfilipe04 <gnr_filipe@hotmail.com>
2022-05-24 10:10:47 -0300

    :hammer: refactor api base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+let baseURL = process.env.NODE_ENV == 'development' ? process.env.NEXT_PUBLIC_DEV_BASE_URL : process.env.NEXT_PUBLIC_PRD_BASE_URL
+
 const api = axios.create({
-    baseURL: 'http://localhost:3333/' 
+    baseURL 
 })
 
 export default api
